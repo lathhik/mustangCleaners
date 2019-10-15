9230061a157822517865a09aabed962c225ceab0
Dorje Tshering <Dorje Tshering>
2019-10-15 14:32:12 +0545

    head file edited

---
+++ resources/views/backend/partials/head.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+
+<!-- Mirrored from demo.dashboardpack.com/kero-html-sidebar-pro/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 15 Oct 2019 07:17:38 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Analytics - This is an example dashboard created using build-in elements and components.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"
+    />
+    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
+    <link rel="icon" href="favicon.ico">
+
+    <!-- Disable tap highlight on IE -->
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <link href="main.07a59de7b920cd76b874.css" rel="stylesheet">
+</head>
+<body>
